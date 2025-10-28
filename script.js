a19429004ce451d71e0ed62cc6ab798bff86ccd4
iamhaziqazhar <haziqazhar1m@gmail.com>
2025-10-28 13:49:18 +0500

    Question 1 and 2 complete

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-let bgChange= document.querySelector("#theme");
-let change="dark";
-let body=document.querySelector("body");
+// <!-- question # 1
 
+// let bgChange= document.querySelector("#theme");
+// let change="dark";
+// let body=document.querySelector("body");
 
-const change2= addEventListener("click", ()=>{
-console.log("haziq")
-if(change==="dark"){body.style.backgroundColor="black"
-    change="light";
-}
-else {body.style.backgroundColor="white"
-    change="dark"
-}  ;
-})
+// const change2= addEventListener("click", ()=>{
+// console.log("haziq")
+// if(change==="dark"){body.style.backgroundColor="black"
+//     change="light";
+// }
+// else {body.style.backgroundColor="white"
+//     change="dark"
+// }  ;
+// })
+
+// <!-- question # 2
+
+// let ans1 = document.querySelector(".answer1");
+// let answer = document.querySelector("#hide");
+// ans1.addEventListener("click", () => {
+// if  (answer.style.display==="none"|| !answer.style.display) {
+//     answer.style.display="flex";
+// } 
+// else { 
+//     answer.style.display="none";
+// }
+// });
